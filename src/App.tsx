272a9bbbc3001537ea16c8d40a657decdfcd0258
Tracey Roberts <traceynicole71@yahoo.com>
2026-04-28 09:58:05 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import AdvancedCounter from './components/AdvancedCounter';
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App-container">
+      <AdvancedCounter />
+    </div>
+  );
+}
+export default App
